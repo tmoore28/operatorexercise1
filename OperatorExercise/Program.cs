fb5495a245d8574b04d2495938e46640f589dcf8
tmoore28 <mooretylor28@gmail.com>
2021-11-18 23:31:20 -0500

    operator exercise one

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4)
+            Console.WriteLine($"17/4 is {quotient},remainder {remainder}");            
+        }
+    }
+}
